154609f27d49487c0564707c2ffb837f4b92af17
Manish <chandrajob365@gmail.com>
2017-12-14 15:20:39 +0530

    Update variuos error scenarios
    Check for reuse of genrated token
    
    Disconnect event on ony peer
    
    token generated using shortId
    
    unregisterToken event handling

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express()
 const path = require('path')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const shortid = require('shortid')
 const pairedSockets = {}
 const socketDetails = {}
+const tokenGenerator = {}
 app.use(express.static(path.join(__dirname, '/public')))
 
 server.listen(process.env.PORT || 8080, () => {
@@ -13,31 +15,50 @@ server.listen(process.env.PORT || 8080, () => {
 
 io.on('connection', socket => {
   let token = socket.handshake.query.token
+  socketDetails[socket.id] = {socket: socket}
   console.log('[connection] token = ', token)
-  updateSocketAndPairs(socket, token)
-  emitPeerMsg(socket, token)
+  if (token) createPairs(socket, token)
+  // emitPeerMsg(socket, token)
   generateToken(socket)
   activateSenderProfile(socket)
   connectPeers(socket)
   msgToSender(socket)
   msgToReceiver(socket)
-  scrapToken(socket)
+  unregisterToken(socket)
   disconnect(socket)
 })
 
-const isExist = token => {
-  return (token && socketDetails[token])
+const isPairExist = token => {
+  console.log('[isPairExist] pairedSockets[token]', pairedSockets[token])
+  return pairedSockets[token] ? true : false
 }
 
-const updateSocketAndPairs = (socket, token) => {
-  if (isExist(token)) {
-    console.log('[updateSocketAndPairs] isExit true')
-    socketDetails[socket.id] = {socket: socket}
-    pairedSockets[token] = {senderId: token, recieverId: socket.id}
+const createPairs = (socket, token) => {
+  console.log('[createPairs] token = ', token)
+  console.log('[createPairs] tokenGenerator = ', tokenGenerator)
+  console.log('[createPairs] tokenGenerator[%s] = %s', token, tokenGenerator[token])
+  if (tokenGenerator[token]) {
+    if (!isPairExist(token)) {
+      console.log('[!isPairExist] Entry')
+      pairedSockets[token] = {senderId: tokenGenerator[token], recieverId: socket.id}
+      socketDetails[socket.id].token = token
+      emitPeerMsg(socket, token)
+    } else {
+      console.log('[isPairExist]')
+      notifyInvalidToken(socket, 'Invalid Token', token)
+    }
   } else {
-    console.log('[updateSocketAndPairs] isExit False')
-    socketDetails[socket.id] = {socket: socket}
+    console.log('Sender/Generator of token (%s) doesn\'t exist', token)
+    notifyInvalidToken(socket, 'Expired Token', token)
   }
+}
+
+const notifyInvalidToken = (socket, type, token) => {
+  console.log('[notifyInvalidToken] token : ', token)
+  socket.emit('invalid', {
+    type: type,
+    token: token
+  })
 }
 
 const emitPeerMsg = (socket, token) => {
@@ -56,8 +77,12 @@ const emitPeerMsg = (socket, token) => {
 
 const generateToken = socket => {
   socket.on('generateToken', msg => {
+    let token = shortid.generate()
+    tokenGenerator[token] = socket.id
+    console.log('[generateToken] tokenGenerator = ', tokenGenerator)
+    socketDetails[socket.id]['generatedToken'] = token // For File sender token is named as generated Token
     socket.emit('tokenGenerated', {
-      token: socket.id
+      token: token
     })
   })
 }
@@ -75,9 +100,15 @@ const activateSenderProfile = socket => {
 
 const connectPeers = socket => {
   socket.on('connectPeers', msg => {
-    pairedSockets[msg.token] = {senderId: msg.token, recieverId: socket.id}
-    emitPeerMsg(socket, msg.token)
+    createPairs(socket, msg.token)
+    let fileSender = tokenGenerator[msg.token]
+    pairedSockets[msg.token] = {senderId: fileSender, recieverId: socket.id}
+    if (msg.token && isValidToken(socket, msg.token)) emitPeerMsg(socket, msg.token)
   })
+}
+
+const isValidToken = (socket, token) => {
+  return (tokenGenerator[token] && pairedSockets[token])
 }
 
 const msgToSender = socket => {
@@ -94,13 +125,37 @@ const msgToReceiver = socket => {
   })
 }
 
-const scrapToken = socket => {
-  // delete the token object from pariedSockets once file transfer is done
+const unregisterToken = socket => {
+  socket.on('unregisterToken', () => {
+    console.log('[unregisterToken] Entry........')
+    let token = socketDetails[socket.id].token
+    socketDetails[socket.id].token = '' // reset token key from fileReceiver socketDetail's object
+    if (pairedSockets[token]) {
+      /*
+         1 > delete pairedSocket object for that token
+         2 > reset token in fileSender socketDetail's object
+         3 > delete token - sender mapping Object
+      */
+      let fileSendersId = pairedSockets[token].senderId
+      delete pairedSockets[token]
+      socketDetails[fileSendersId].token = ''
+      delete tokenGenerator[token]
+    }
+  })
 }
 
 const disconnect = socket => {
   socket.on('disconnect', () => {
+    let token = socketDetails[socket.id].token
     delete socketDetails[socket.id]
-    if (pairedSockets[socket.id]) delete pairedSockets[socket.id]
+    // Below case only if fileReceiver is getting disconnected
+    if (token && pairedSockets[token]) {
+      /* if token has already been shared and another client uses it
+       then get the id of fileSender from pairedSocket
+       and set token as empty in senderSocketDetail Object  and delete token - sender mapping Object */
+      let fileSendersId = pairedSockets[token].senderId
+      socketDetails[fileSendersId].token = ''
+      delete tokenGenerator[token]
+    }
   })
 }
